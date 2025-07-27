d87f41701f5d0e9594f19c30b603cf71d9dcb7da
Alexis am <alex.am69@proton.me>
2025-07-27 17:22:33 +0200

    Nouveau commit 2 pour la branche test2

--- src/main/java/com/test/traducteur/api/controllers/TraducteurController.java
+++ src/main/java/com/test/traducteur/api/controllers/TraducteurController.java
@@ -28,6 +28,7 @@ public class TraducteurController {
 		return traducteurService.traduireJavanaisVersFrancais(texteATraduireDto);
 		
 		// Nouveau commit pour la branch test2 => Modif pour tester la résolution de conflit
+		// Nouveau commit pour la branch test2
 	}
 	
 }
